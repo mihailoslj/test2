1a690eaa5fae919148e027530e57db36208436fa
mihailoslj <samozagit@gmail.com>
2022-12-01 02:46:33 +0100

    treci komit

--- src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -12,6 +12,6 @@ package com.mycompany.mavenproject1;
 public class Mavenproject1 {
 
     public static void main(String[] args) {
-        System.out.println("Cao svete");
+        System.out.println("Cao svete sadasda ");
     }
 }
